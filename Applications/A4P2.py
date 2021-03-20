4066e0853db905d58f71a48940d8fba30ca95e2f
Qasim Khawaja <q.khawaja@yahoo.com>
2021-03-20 16:09:30 +0500

    fix: setup build and code for 1,2, and 4

---
+++ Applications/A4P2.py
@@ -0,0 +1,5 @@
+def main():
+    print("Implement P2 here")
+    
+if __name__ == "__main__":
+    main()
---
+++ Applications/A4P4.py
@@ -0,0 +1,5 @@
+def main():
+    print("Implement P4 here")
+    
+if __name__ == "__main__":
+    main()
